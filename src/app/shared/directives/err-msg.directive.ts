f9e158afd149f1c5b109fbbfe6bdc7faa8da61b6
alejahndro02 <alejahndro02@gmail.com>
2022-03-22 14:39:00 -0600

    se declara un decraador input, de inyecta la dependenca de elementREf y se declara un metodo que obtiene el color

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -1,17 +1,28 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, 
+         ElementRef, 
+         Input, 
+         OnInit       } from '@angular/core';
 
 @Directive({
   selector: '[errorMsj]'
 })
 export class ErrMsgDirective implements OnInit{
+  @Input() color :string='red'
+  
+  htmlElement :ElementRef<HTMLElement>
 
-  constructor() {
-    console.log('cost directive');
-    
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el   
+
    }
   ngOnInit(): void {
     console.log('oninit directive');
+    this.setColor()
     
+  }
+
+  setColor(){
+    this.htmlElement.nativeElement.style.color = this.color
   }
 
 }

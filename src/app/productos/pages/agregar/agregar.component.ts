ac836b901ae61c263a120e639ab0eb562dca6794
alejahndro02 <alejahndro02@gmail.com>
2022-03-22 16:13:42 -0600

    se declaro una propiedad texto1 de tipo string

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,5 +1,5 @@
 import { Component, 
-         OnInit         } from '@angular/core';
+         OnInit,        } from '@angular/core';
 import { FormBuilder, 
          FormGroup, 
          Validators,    } from '@angular/forms';
@@ -11,16 +11,23 @@ import { FormBuilder,
   ]
 })
 export class AgregarComponent implements OnInit {
+  texto1:string="Quetzalli"
   // Se configuran las propiedades del formulario 
   miFormulario: FormGroup = this.fb.group({
     nombre:['',Validators.required]
   })
   constructor(private fb:FormBuilder) { }
 
+
   ngOnInit(): void {
   }
   // Controla el msj de error
   msjError(campo:string):boolean{
     return this.miFormulario.get(campo)?.invalid || false
+  }
+  cambiarNombre(){
+    this.texto1="Quetza"
+    console.log(this.texto1);
+    
   }
 }

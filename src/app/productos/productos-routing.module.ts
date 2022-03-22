770878a36b9bb23f46d927571925c61ad272e18f
alejahndro02 <alejahndro02@gmail.com>
2022-03-22 08:13:55 -0600

    se crearon las rutas del proyecto

--- src/app/productos/productos-routing.module.ts
+++ src/app/productos/productos-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule         } from '@angular/core';
+import { RouterModule, 
+         Routes           } from '@angular/router';
+import { AgregarComponent } from './pages/agregar/agregar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path:'agregar',
+        component: AgregarComponent
+      },
+      {
+        path:'**',
+        redirectTo: 'agregar'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

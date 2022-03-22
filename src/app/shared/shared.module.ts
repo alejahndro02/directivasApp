26cad6ca1b07d00244208f7b1ae1a89b939159c5
alejahndro02 <alejahndro02@gmail.com>
2022-03-22 11:39:26 -0600

    Se importo y se exporto la directiva para poder usarla en otro modulo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule        } from '@angular/core';
+import { CommonModule    } from '@angular/common';
+// directia personalizaa
+import { ErrMsgDirective } from './directives/err-msg.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrMsgDirective
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    ErrMsgDirective
   ]
 })
 export class SharedModule { }

e302823924ef36d1d89dbbc60d61d207fed826fc
alejahndro02 <alejahndro02@gmail.com>
2022-03-23 06:26:04 -0600

    Se declaro un apropiedad llamada _Valido de tipo boolean, se declaro colo un setter

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -7,9 +7,14 @@ import { Directive,
   selector: '[errorMsj]'
 })
 export class ErrMsgDirective implements OnInit{
+  
   htmlElement :ElementRef<HTMLElement>
+  
   private _color:string='red'
   private _mensaje:string='Este campo es Requerido'
+  private _valido:boolean=true
+
+  
   // se declara un setter para manjear el cambio de color 
   @Input() set color(valor:string){
     this._color = valor 
@@ -21,18 +26,22 @@ export class ErrMsgDirective implements OnInit{
     this.setMsg()
   }
   
-
+  @Input() set valido(valor:boolean){
+    if(valor){
+      this.htmlElement.nativeElement.classList.add('hiden')
+    }else{
+      this.htmlElement.nativeElement.classList.remove('hiden')
+    }
+  }
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el   
 
    }
 
-
   ngOnInit(): void {
     this.addClass()
     this.setColor()
     this.setMsg()
-
   }
   
   setColor(){
